591d1db015cd127dc3ae6a5f3b10adf6f8949cc1
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 17:39:32 +0530

    first commit

---
+++ JAVA PROGRAMMING/src/demoJAVA/LogicalOperator.java
@@ -0,0 +1,28 @@
+package demoJAVA;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// Logical Operator
+		
+		//&& AND operator
+		float a = 25, b= 45,c=25;
+		if(a==b&&a==c) {
+			System.out.println("True");	
+		}else {
+			System.out.println("false");
+			
+		}
+		//|| OR operator
+		if(c==a||b==a) {
+			System.out.println("anyone true");
+		}
+		
+		// ! NOT operator
+		if(b!=c) {
+			System.out.println("true");
+		}
+		
+	}
+
+}
