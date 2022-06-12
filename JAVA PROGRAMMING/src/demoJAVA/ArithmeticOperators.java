038e3de6ea0b69d8c79f87093ad7c6341736b790
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 16:47:37 +0530

    first commit

---
+++ JAVA PROGRAMMING/src/demoJAVA/ArithmeticOperators.java
@@ -0,0 +1,29 @@
+package demoJAVA;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// Arithmetic operators
+		//+ addition operator or unary plus
+		//- subtraction operator or unary minus
+		// / division operator
+		//* multiplication operator
+		// % modulus operator
+		
+		// program example
+		//initializing
+		int a = 25;
+		int b = 40;
+		
+		//Output statement
+		
+		System.out.println("Sum of a and b is:"+(a+b));
+		System.out.println("difference of a and b is:"+(a-b));
+		System.out.println("multiplication of a and b is:"+(a*b));
+		System.out.println("division of a and b is:"+(b/a));
+		System.out.println("Modulus of a and b is:"+(a%b));
+		
+
+	}
+
+}
