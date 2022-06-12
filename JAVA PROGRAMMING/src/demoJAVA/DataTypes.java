c2a0b28ee9e09ac95939b57b12e59a089011391a
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 16:32:14 +0530

    first commit

---
+++ JAVA PROGRAMMING/src/demoJAVA/DataTypes.java
@@ -0,0 +1,22 @@
+package demoJAVA;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Data types in JAVA
+		int a = 25;
+		String b = "hari";
+		double c = 67.00;
+		long d = 786;
+		boolean e = false;
+		
+		// to print the output
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+	}
+
+}
