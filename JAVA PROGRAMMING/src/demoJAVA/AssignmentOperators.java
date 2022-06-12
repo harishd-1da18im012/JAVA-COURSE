f657dfa07de1cb358dcbf29b678a835a4d9799a2
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 17:15:58 +0530

    first commit

---
+++ JAVA PROGRAMMING/src/demoJAVA/AssignmentOperators.java
@@ -0,0 +1,17 @@
+package demoJAVA;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		// Assignment operators
+		 float a = 350.98F,b = 25F , n = 35F;
+		 System.out.println("a=a+1:"+(a += 1));
+		 System.out.println("a=a-1:"+(a -= 1));
+		 System.out.println("a=a*(n+1):"+(a *= n+1));
+		 System.out.println("a=a/(n+1):"+(a /= n+1));
+		 System.out.println("a=a%b:"+(a %= b));
+		 
+
+	}
+
+}
