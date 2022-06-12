4bd6f60921f5ea8d9ac671ca664078df169eb8e2
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 19:56:39 +0530

    first commit

---
+++ JAVA PROGRAMMING/src/demoJAVA/InstanceofOperator.java
@@ -0,0 +1,12 @@
+package demoJAVA;
+
+public class InstanceofOperator {
+
+	public static void main(String[] args) {
+		// InstanceOf operator is a special type of operator
+		String person = new String("hi") ;
+		System.out.println(person instanceof String);
+
+	}
+
+}
