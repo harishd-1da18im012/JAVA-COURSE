2e0ac06854af145e6ea9051f1afc4163968c2cb3
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 20:56:26 +0530

    Second commit

---
+++ JAVA PROGRAMMING/src/controlStatements/IfElseIfDemo1.java
@@ -0,0 +1,33 @@
+package controlStatements;
+
+import java.util.Scanner;
+
+public class IfElseIfDemo1 {
+
+	public static void main(String[] args) {
+		
+		// If-ElseIf-Else statement
+		
+		Scanner sc = new Scanner(System.in);
+		float num1 = sc.nextFloat();
+		float num2 = sc.nextFloat();
+		
+		
+		// largest between 2 numbers
+		
+		if (num1>num2) {
+			System.out.println("num1 is greater than num2");
+			
+		}
+		else if (num1==num2) {
+			System.out.println("num1 is equal to num2");
+		
+		}
+		else {
+			System.out.println("num1 is less than num2");
+		}
+		
+
+	}
+
+}
