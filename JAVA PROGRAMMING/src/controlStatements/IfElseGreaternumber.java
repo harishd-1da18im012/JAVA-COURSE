04d508d40e5da6526e04c2bf75038e51a92d1aad
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-12 20:38:49 +0530

    Second commit

---
+++ JAVA PROGRAMMING/src/controlStatements/IfElseGreaternumber.java
@@ -0,0 +1,26 @@
+package controlStatements;
+
+import java.util.Scanner;
+
+public class IfElseGreaternumber {
+
+	public static void main(String[] args) {
+		
+		// to check the largest number taking input from user
+		
+		Scanner sc = new Scanner(System.in);
+		float num1 = sc.nextFloat();
+		Float num2 = sc.nextFloat();
+		
+		//condition to print largest number
+		
+		if (num1>num2) {
+			System.out.println("Num1 is greater");
+		}
+		else {
+			System.out.println("Num2 is greater");
+		}
+
+	}
+
+}
