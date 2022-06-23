1c56106568b69e216bff85ff6b869a7e0b041b5b
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-23 23:05:14 +0530

    Seventh commit

---
+++ JAVA PROGRAMMING/src/ExceptionHandling/TestTryCatch1.java
@@ -0,0 +1,17 @@
+package ExceptionHandling;
+
+public class TestTryCatch1 {
+
+	public static void main(String[] args) {
+		
+		try
+		{
+	       int data = 50/0;
+		}
+		catch(ArithmeticException ae)
+		{
+			System.out.println(ae);
+		}
+	}
+
+}
