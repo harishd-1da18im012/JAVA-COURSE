4749651828a6842c2014bc86cbc2b36f56eb4693
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-23 22:08:48 +0530

    Second commit

---
+++ JAVA PROGRAMMING/src/controlStatements/DoWhileloop.java
@@ -0,0 +1,14 @@
+package controlStatements;
+
+public class DoWhileloop {
+
+	public static void main(String[] args) {
+		int i=1;    
+	    do{    
+	        System.out.println(i);    
+	    i++;    
+	    }while(i<=20);
+				}
+		// TODO Auto-generated method stub
+
+	}
