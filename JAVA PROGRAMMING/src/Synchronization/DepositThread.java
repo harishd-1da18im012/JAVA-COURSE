65911c63f6c307d23591d5cb8836615b1f2b5cc0
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-23 22:50:49 +0530

    Fourth commit

---
+++ JAVA PROGRAMMING/src/Synchronization/DepositThread.java
@@ -0,0 +1,30 @@
+package Synchronization;
+
+public class DepositThread extends Thread{
+	
+
+	AccountClass obj;
+	int amt;
+	String msg;
+	
+	public DepositThread(AccountClass obj, int amt,String msg) {	
+		this.obj = obj;
+		this.amt = amt;
+		this.msg=msg;
+	}
+
+	@Override
+	public void run() {
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		//System.out.println("First");
+		obj.deposit(amt);
+	}
+}
+
