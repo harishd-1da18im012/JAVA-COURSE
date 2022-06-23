113ba1a99412eee40eed9d3503afa6e9c4a1eca6
harishd-1da18im012 <104613450+harishd-1da18im012@users.noreply.github.com>
2022-06-23 22:15:48 +0530

    Second commit

---
+++ JAVA PROGRAMMING/src/controlStatements/ForloopHalfPyramid.java
@@ -0,0 +1,28 @@
+package controlStatements;
+
+public class ForloopHalfPyramid {
+	
+	//TODO pyramid problem with for loop
+	
+	public static void main(String[] args) {
+		
+		int rows = 10;
+		
+		//test for condition
+		
+		for (int i = 1; i <= rows; ++i) {
+			
+			for (int j = 1; j <= i; ++j) {
+				
+				System.out.print(" * ");
+				
+			}
+			
+		System.out.println( );
+		
+		}
+		
+
+	}
+
+}
